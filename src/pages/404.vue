574f8e64be31eaeae9ad690b7f106d6b1c1599d6
rsclarke <rsclrk@pm.me>
2019-05-27 15:42:50 +0800

    custom 404

---
+++ src/pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    
+    <h1 class="text-4xl tracking-wider text-center text-light font-normal mb-4">404 - not found</h1>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404'
+  }
+}
+</script>
+
+<style>
+
+</style>
