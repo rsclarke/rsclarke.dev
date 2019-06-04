2828b8e3277e9aa3702a57d9a3e6369d88b2b704
rsclarke <rsclrk@pm.me>
2019-06-04 09:03:57 +0800

    vulns listing

--- src/pages/Vulns.vue
+++ src/pages/Vulns.vue
@@ -1,17 +1,61 @@
 <template>
   <Layout>
+
+    <h1 class="text-yellow">Vulns</h1>
     
-    <p>
-      Paginate bugs, 10 per page?
-    </p>
+    <template v-if="$page.vulns.totalCount > 0">
+      <div v-for="edge in $page.vulns.edges" :key="edge.node.id">
+          <PostHeader :post="edge.node" />
+          <p>{{edge.node.description}} <g-link :to="edge.node.path">…continue</g-link></p>
+      </div>
+
+      <Pageit :pageInfo="$page.vulns.pageInfo" />
+    </template>
+    <template v-else>
+      <p>No vulns found!</p>
+    </template>
 
   </Layout>
 </template>
 
+<page-query>
+query Vulns($page: Int) {
+  vulns: allVuln(page: $page, perPage: 10) @paginate {
+    totalCount
+    pageInfo {
+        totalPages
+        currentPage
+    }
+    edges {
+      node {
+        __typename
+        title
+        path
+        description
+        timeToRead
+        date (format: "YYYY/MM/DD")
+        tags {
+          id
+          title
+          path
+        }
+      }
+    }    
+  }
+}
+</page-query>
+
 <script>
+import PostHeader from '~/components/PostHeader'
+
 export default {
-  metaInfo: {
-    title: 'Latest Vulnerabilities'
+  components:{
+    PostHeader
+  },
+  metaInfo() {
+    return {
+      title: 'Latest Vulnerabilities'
+    }
   }
 }
 </script>
