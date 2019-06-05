f45df117aeb820134084f42a7bba7862a5ca382e
rsclarke <rsclrk@pm.me>
2019-06-05 12:34:27 +0800

    remove moment, not needed

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,8 +5,6 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const moment = require('moment')
-
 module.exports = function (api) {
   api.loadSource(store => {
     // Use the Data store API here: https://gridsome.org/docs/data-store-api
